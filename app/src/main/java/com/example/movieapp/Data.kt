a7d38dd3aae97c06c437a797005a0c93b48ed6af
Pavel Petukhov <pavel.petukhov@tele2.ru>
2021-07-17 23:32:13 +0300

    wip

--- app/src/main/java/com/example/movieapp/Data.kt
+++
@@ -1,7 +0,0 @@
-package com.example.movieapp
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-class Data (var buttonNumber: Int):Parcelable
--- app/src/main/java/com/example/movieapp/DetailsActivity.kt
+++ app/src/main/java/com/example/movieapp/DetailsActivity.kt
@@ -3,9 +3,18 @@ package com.example.movieapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class DetailsActivity: AppCompatActivity() {
+/**
+ * Экран детального описание фильма
+ */
+class DetailsActivity : AppCompatActivity() {
+    /**
+     * В методе определяется layout для Activity, исходя из элемента, по которму был выполнен клик
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        val viewId = intent.getIntExtra("view", 0)
+        if (viewId != 0) {
+            setContentView(viewId)
+        }
     }
 }
